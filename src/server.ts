e1a82459994e2439137273da001b8399139da722
fabio-hidetoshi <fhaperes@gmail.com>
2024-07-10 17:13:24 -0300

    feat: Add new routes

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,34 @@
 import fastify from "fastify";
-import { prisma } from "./lib/prisma";
+import cors from "@fastify/cors";
 import { createTrip } from "./routes/create-trip";
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
+import { confirmParticipant } from "./routes/confirm-participants";
+import { createActivity } from "./routes/create-activity";
+import { getActivies } from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
 
-const app = fastify()
+const app = fastify();
+
+app.register(cors, {
+  origin: "*",
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.register(createTrip)
+app.register(createTrip);
+app.register(confirmTrip);
+app.register(confirmParticipant);
+app.register(createActivity);
+app.register(getActivies);
+app.register(createLink);
+app.register(getLinks);
 
-
-app.listen({port:3030}).then(() => {
-    console.log("Server running");
-    
-})
+app.listen({ port: 3030 }).then(() => {
+  console.log("Server running");
+});
